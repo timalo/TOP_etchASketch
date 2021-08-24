b7435db7301ace26a52b3c2b3e9301062641dfc3
timalo <tmalo@student.oulu.fi>
2021-08-24 19:48:26 +0300

    Added a clear button and the possibility to resize the canvas

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 function createDivs(amount) {
     //This function creates a grid of divs inside the container
     //the grid will have a size of (amount x amount)
+    if(amount > 100) {
+        const alertText = document.querySelector('#alertText');
+        alertText.textContent = "Dimension too large! Please use a value between 1-100"
+        amount = 100;
+    }
+    else if(amount <= 0) {
+        const alertText = document.querySelector('#alertText');
+        alertText.textContent = "Do you even draw?";
+        return;
+    }
+    else {
+        const alertText = document.querySelector('#alertText');
+        alertText.textContent = "";
+    }
     const container = document.querySelector('#container');
     for(let i = 0; i < (amount * amount); i++) {
         var div = document.createElement("div");
+        div.style.height = `${640/amount}px`; //calculate height and width for the square using the amount of squares
+        div.style.width = `${640/amount}px`; //lol same I guess
         div.classList.add("box");
         container.appendChild(div);
     }
@@ -12,7 +28,21 @@ function createDivs(amount) {
             item.classList.add("painted");
         })
     });
-
+    const dimensionText = document.querySelector('#currentDimension');
+    dimensionText.textContent = `Current dimensions: ${amount} x ${amount}`;
 }
 
-createDivs(16);
+function clearCanvas() {
+    const boxes = document.querySelectorAll('.box').forEach(item => {
+        item.remove();
+    });
+    let canvasSize = Math.floor(prompt("Give new dimension for the canvas (1-100)"));
+    createDivs(canvasSize);
+}
+
+createDivs(16); //initialize the grid with 16x16
+
+document.querySelector('#clearButton').addEventListener("click", function() {
+    clearCanvas();
+});
+
